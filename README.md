7c1693379f79124202fcf0f0c6b53e8edff0ebef
MimFSD <mimislam878@gmail.com>
2024-08-17 18:40:18 +0600

    all done

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-# Pagination
 
-This is the backend part of the Pagination built with React.js.
+* ShopEase -- E-commerce website -Server site
 
-## Features
 
-- Product Listing with Pagination
-- Product Searching
-- Product Filtering by Category, Brand, and Price Range
-- Product Sorting by Price and Date
-- User Authentication (Google and Email/Password) using Firebase
-- Responsive Design (Mobile-First)
-- Fixed-size Product Cards
+
--- index.js
+++ index.js
@@ -1,97 +1,141 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
 const port = process.env.PORT || 5000;
-const products = require("./products.json");
-const app = express();
-// middleware
-app.use(express.json());
-//Must remove "/" from your production URL
-app.use(
-    cors({
-        origin: ["http://localhost:5173"],
-    }),
-);
-const uri = process.env.MONGODB_URI;
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jecns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        const db = client.db("ProductPulseDB");
-        const productsCollection = db.collection("products");
-        app.get("/seed", async (req, res) => {
-            await productsCollection.deleteMany();
-            await productsCollection.insertMany(products);
-            return res.send({ message: "seed successfully" });
-        });
-        app.get("/products", async (req, res) => {
-            const search = req.query.search;
-            const sort = req.query.sort;
-            const brandName = req.query.brandName;
-            const categoryName = req.query.categoryName;
-            const price = parseInt(req.query.price);
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const ProductsCollection = client.db('Jobtask').collection('products')
+    const BransCollection = client.db('Jobtask').collection('brand')
+    const CategoryCollection = client.db('Jobtask').collection('category')
 
-            let options = {};
-            if (sort === "Low to High") {
-                options = { sort: { price: 1 } };
-@@ -65,6 +67,9 @@ async function run() {
-            if (categoryName) {
-                query.category = categoryName;
-            }
-            if (price) {
-                query.price = { $lt: price };
-            }
+   //All Product
+    app.get('/products', async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
 
-            const size = parseInt(req.query.size);
-            const page = parseInt(req.query.page);
-@@ -80,6 +85,7 @@ async function run() {
-            const search = req.query.search;
-            const brandName = req.query.brandName;
-            const categoryName = req.query.categoryName;
-            const price = parseInt(req.query.price);
-            let query = {};
-            if (search) {
-                query.name = { $regex: search, $options: "i" };
-@@ -90,6 +96,9 @@ async function run() {
-            if (categoryName) {
-                query.category = categoryName;
-            }
-            if (price) {
-                query.price = { $lt: price };
-            }
-            const count = await productsCollection.countDocuments(query);
-            res.send({ count });
-        });
-        app.get("/categorization", async (req, res) => {
-            const brandNames = await productsCollection
-                .aggregate([{ $group: { _id: "$brand" } }, { $project: { _id: 0, brand: "$_id" } }])
-                .toArray();
-            const categories = await productsCollection
-                .aggregate([{ $group: { _id: "$category" } }, { $project: { _id: 0, category: "$_id" } }])
-                .toArray();
-            res.send({ brandNames, categories });
-        });
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+      const brands = req.query.brands || [];
+      const categories = req.query.categories || [];
+      const minPrice = parseFloat(req.query.minPrice);
+      const maxPrice = parseFloat(req.query.maxPrice);
+
+      const sorting = req.query.sorting
+      const search = req.query.search || ''
+
+      const query = {};
+
+      //Searching
+      if (search) {
+        query.productName = { $regex: search, $options: 'i' };
+      }
+
+      //Sorting
+      const options = {
+        sort: {}
+      };
+      if (sorting === 'acs') {
+        options.sort.price = 1;
+      } else if (sorting === 'dcs') {
+        options.sort.price = -1;
+      } else if (sorting === 'date-acs') {
+        options.sort.creationDate = 1;
+      }
+
+      //Filtering
+      if (brands.length > 0) {
+        query.brandName = { $in: Array.isArray(brands) ? brands : [brands] };
+      }
+
+      if (categories.length > 0) {
+        query.category = { $in: Array.isArray(categories) ? categories : [categories] };
+      }
+
+      if (minPrice || maxPrice) {
+        query.price = { $gte: minPrice, $lte: maxPrice };
+      }
+
+
+      const result = await ProductsCollection
+        .find(query)
+        .sort(options.sort)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+   //Count Product for pages
+    app.get('/products-count', async (req, res) => {
+      const brands = req.query.brands || [];
+      const categories = req.query.categories || [];
+      const minPrice = parseFloat(req.query.minPrice) || 0;
+      const maxPrice = parseFloat(req.query.maxPrice) || Number.MAX_VALUE;
+      const search = req.query.search || ''
+      const query = {};
+      if (search) {
+        query.productName = { $regex: search, $options: 'i' };
+      }
+
+      if (brands.length > 0) {
+        query.brandName = { $in: Array.isArray(brands) ? brands : [brands] };
+      }
+
+      if (categories.length > 0) {
+        query.category = { $in: Array.isArray(categories) ? categories : [categories] };
+      }
+
+      if (minPrice || maxPrice) {
+        query.price = { $gte: minPrice, $lte: maxPrice };
+      }
+
+      const count = await ProductsCollection.countDocuments(query);
+      res.send({ count });
+    })
+
+    //Brands
+    app.get('/All-brand', async (req, res) => {
+      const result = await BransCollection.find().toArray()
+      res.send(result)
+    })
+
+    //Category 
+    app.get('/All-category', async (req, res) => {
+      const result = await CategoryCollection.find().toArray()
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
-app.get("/", (req, res) => {
-    res.send("Product Pulse is running...");
-});
+
+
+app.get('/', (req, res) => {
+  res.send('Assignmnet-14 is running')
+})
+
 app.listen(port, () => {
-    console.log("Product Pulse server running on", port);
-});
+  console.log(`Assignmnet-14 is running on ${port}`);
+})
