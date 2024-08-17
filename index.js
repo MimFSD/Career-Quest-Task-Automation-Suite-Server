06becbf881c3b1b54f887b00aabbafa821dc2107
MimFSD <mimislam878@gmail.com>
2024-08-17 17:44:44 +0600

    add: products seeding api created. packages updated

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000;
 
-// middle ware
-app.use(cors({
-    origin: ['http://localhost:5173']
-}))
-app.use(express.json())
+const BransCollection = client.db('Jobtask').collection('brand')
+const CategoryCollection = client.db('Jobtask').collection('category')
 
 
 
-
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.rjjtc94.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+app.get('/products', async (req, res) => {
+  const size = parseInt(req.query.size);
+  const page = parseInt(req.query.page) - 1;
+  const brands = req.query.brands || [];
+  const categories = req.query.categories || [];
+  const minPrice = parseFloat(req.query.minPrice);
+  const maxPrice = parseFloat(req.query.maxPrice);
+  const sorting = req.query.sorting
+  const search = req.query.search || ''
+  const query = {};
+  if (search) {
+    query.productName = { $regex: search, $options: 'i' };
+  }
+  const options = {
+    sort: {}
+  };
+  if (sorting === 'acs') {
+    options.sort.price = 1;
+  } else if (sorting === 'dcs') {
+    options.sort.price = -1;
+  } else if (sorting === 'date-acs') {
+    options.sort.creationDate = 1;
+  }
+  if (brands.length > 0) {
+    query.brandName = { $in: Array.isArray(brands) ? brands : [brands] };
+  }
+  if (categories.length > 0) {
+    query.category = { $in: Array.isArray(categories) ? categories : [categories] };
+  }
+  if (minPrice || maxPrice) {
+    query.price = { $gte: minPrice, $lte: maxPrice };
+  }
+  const result = await ProductsCollection
+    .find(query)
+    .sort(options.sort)
+    .skip(page * size)
+    .limit(size)
+    .toArray();
+  res.send(result);
 });
-
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        const productsCollection = client.db('jobTask').collection('products')
-
-
-
-        // products api
-        app.get('/products', async (req, res) => {
-            const result = await productsCollection.find().toArray();
-            res.send(result)
-        })
-
-
-
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
-}
-run().catch(console.dir);
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Job task is running')
+app.get('/products-count', async (req, res) => {
+  const brands = req.query.brands || [];
+  const categories = req.query.categories || [];
+  const minPrice = parseFloat(req.query.minPrice) || 0;
+  const maxPrice = parseFloat(req.query.maxPrice) || Number.MAX_VALUE;
+  const search = req.query.search || ''
+  const query = {};
+  if (search) {
+    query.productName = { $regex: search, $options: 'i' };
+  }
+  if (brands.length > 0) {
+    query.brandName = { $in: Array.isArray(brands) ? brands : [brands] };
+  }
+  if (categories.length > 0) {
+    query.category = { $in: Array.isArray(categories) ? categories : [categories] };
+  }
+  if (minPrice || maxPrice) {
+    query.price = { $gte: minPrice, $lte: maxPrice };
+  }
+  const count = await ProductsCollection.countDocuments(query);
+  res.send({ count });
 })
-
-app.listen(port, () => {
-    console.log(`Job task running on port ${port}`)
+//Brands
+app.get('/All-brand', async (req, res) => {
+  const result = await BransCollection.find().toArray()
+  res.send(result)
 })
