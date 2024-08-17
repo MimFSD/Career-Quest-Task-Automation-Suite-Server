bb92c378ab56ef25ac1785835fb81358a0cf287b
MimFSD <mimislam878@gmail.com>
2024-08-17 17:57:20 +0600

    filter implement & main task finished

--- index.js
+++ index.js
@@ -29,17 +29,49 @@ async function run() {
         // Send a ping to confirm a successful connection
         const db = client.db("ProductPulseDB");
         const productsCollection = db.collection("products");
-
         app.get("/seed", async (req, res) => {
-            await productsCollection.deleiteMany();
             await productsCollection.deleteMany();
             await productsCollection.insertMany(products);
             return res.send({ message: "seed successfully" });
         });
-@@ -77,6 +77,16 @@ async function run() {
+        app.get("/products", async (req, res) => {
+            const search = req.query.search;
+            const sort = req.query.sort;
+            const brandName = req.query.brandName;
+            const categoryName = req.query.categoryName;
+            const price = parseInt(req.query.price);
+
+            let options = {};
+            if (sort === "Low to High") {
+                options = { sort: { price: 1 } };
+@@ -65,6 +67,9 @@ async function run() {
+            if (categoryName) {
+                query.category = categoryName;
+            }
+            if (price) {
+                query.price = { $lt: price };
+            }
+
+            const size = parseInt(req.query.size);
+            const page = parseInt(req.query.page);
+@@ -80,6 +85,7 @@ async function run() {
+            const search = req.query.search;
+            const brandName = req.query.brandName;
+            const categoryName = req.query.categoryName;
+            const price = parseInt(req.query.price);
+            let query = {};
+            if (search) {
+                query.name = { $regex: search, $options: "i" };
+@@ -90,6 +96,9 @@ async function run() {
+            if (categoryName) {
+                query.category = categoryName;
+            }
+            if (price) {
+                query.price = { $lt: price };
+            }
+            const count = await productsCollection.countDocuments(query);
             res.send({ count });
         });
-
         app.get("/categorization", async (req, res) => {
             const brandNames = await productsCollection
                 .aggregate([{ $group: { _id: "$brand" } }, { $project: { _id: 0, brand: "$_id" } }])
@@ -49,15 +81,17 @@ async function run() {
                 .toArray();
             res.send({ brandNames, categories });
         });
-       
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-     
-  }
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 app.get("/", (req, res) => {
-  res.send("Product Pulse is running...");
+    res.send("Product Pulse is running...");
 });
 app.listen(port, () => {
-  console.log("Product Pulse server running on", port);
+    console.log("Product Pulse server running on", port);
 });
